b2e95fc5128366d244b58806036b3ff3adfe6fb3
Yang Liu <lywander@gmail.com>
2012-10-26 17:26:03 -0700

    Engine x32 works. TODO: template

--- InteractiveSpace.EngineController/NativeWrappers/CommandDllWrapper.cs
+++ InteractiveSpace.EngineController/NativeWrappers/CommandDllWrapper.cs
@@ -10,58 +10,58 @@ namespace InteractiveSpace.EngineController.NativeWrappers
     {
         private const string DLL_NAME = "InteractiveSpaceEngine.dll";
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe void engineRun();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe void engineStop(IntPtr stoppedCallback);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe int getRGBWidth();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe int getRGBHeight();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe int getDepthWidth();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention = CallingConvention.Cdecl)]
         public static extern unsafe int getDepthHeight();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void setOmniTouchParameters(double fingerMinWidth, double fingerMaxWidth, double fingerMinLength, double fingerMaxLength, double fingerRisingThreshold, double fingerFallingThreshold, double clickFloodMaxGrad);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void systemCalibrationStart();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void systemCalibrationStop();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void systemCalibrationDetectChessboardCorner(IntPtr onFinishedCallback, FloatPoint3D* refCorners, int rows, int cols);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void systemCalibrationCalibrateDepthCamera(FloatPoint3D* depthCorners, FloatPoint3D* refCorners, int cornerCount);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void transformPoints(FloatPoint3D* srcPoints, FloatPoint3D* dstPoints, int pointNum, CalibratedCoordinateSystem srcSpace, CalibratedCoordinateSystem dstSpace);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe FloatPoint3D transformPoint(FloatPoint3D srcPoint, CalibratedCoordinateSystem srcSpace, CalibratedCoordinateSystem dstSpace);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void registerHandEventCallbacks(IntPtr handMoveCallback, IntPtr handCapturedCallback, IntPtr handLostCallback);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void registerEngineUpdateCallback(IntPtr engineUpdateCallback);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void startRecording(byte* filepath);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void stopRecording();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void setOmniTouchCropping(int left, int top, int right, int bottom);
     }
 }
--- InteractiveSpace.EngineController/NativeWrappers/ResultsDllWrapper.cs
+++ InteractiveSpace.EngineController/NativeWrappers/ResultsDllWrapper.cs
@@ -10,35 +10,35 @@ namespace InteractiveSpace.EngineController.NativeWrappers
     {
         private const string DLL_NAME = "InteractiveSpaceEngine.dll";
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe ReadLockedWrapperPtr lockFactoryImage(ImageProductType product);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe void releaseReadLockedWrapperPtr(ReadLockedWrapperPtr imgPtr);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe ReadLockedWrapperPtr lockFingers(int* fingerNum);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe ReadLockedWrapperPtr lockHands(int* handNum);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe ReadLockedWrapperPtr lockCalibrationRGBImage();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe ReadLockedWrapperPtr lockCalibrationDepthImage();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe ReadLockedWrapperPtr lockFingerEvents(int* fingerNum, long* frame);
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe long getEngineFrameCount();
 
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe float getFPS();
 
         /*
-        [DllImport(DLL_NAME)]
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl)]
         public static extern unsafe double getKinectSoundAngle();
          */
     }
